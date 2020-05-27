31095a2515a141dbd299b5db0a6396749c46f4e5
Aleks <Aleks@AleKs>
2020-05-27 19:50:29 +0300

    Initial commit

--- AppExcelExp/src/appReadData/ReadExcelFaile.java
+++ AppExcelExp/src/appReadData/ReadExcelFaile.java
@@ -2,36 +2,59 @@ package appReadData;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 
 public class ReadExcelFaile {
 
+    public static final int ID_COLUMN_NUMBER = 0;
+    public static final int NAME_COLUMN_ASIAN = 1;
+    public static final int NAME_COLUMN_EUROPA = 2;
+
     public static void main(String[] args) throws IOException {
         FileInputStream fis = new FileInputStream("123.xls");
         Workbook wb = new HSSFWorkbook(fis);
-        //выбор листа _ строки _ столбца
-//        String result0 = wb.getSheetAt(0).getRow(1).getCell(1).getStringCellValue();
-//        String result1 = wb.getSheetAt(0).getRow(1).getCell(2).getStringCellValue();
-//        String result2 = wb.getSheetAt(0).getRow(1).getCell(3).getStringCellValue();
-//        String result3 = wb.getSheetAt(0).getRow(1).getCell(4).getStringCellValue();
-//        String result4 = wb.getSheetAt(0).getRow(1).getCell(5).getStringCellValue();
-//        String result5 = wb.getSheetAt(0).getRow(1).getCell(6).getStringCellValue();
-// преобразование с вновь введеным методом, который определит формат считываемого знвчения
-        String result0 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(1));
-        String result1 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(3));
-        String result2 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(4));
-        String result3 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(5));
-        String result4 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(6));
-        String result5 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(2));
+        HSSFSheet sheet = (HSSFSheet) wb.getSheetAt(0);
 
-        System.out.println(result0 + "->" + result1 + "->" + result2 + "->" + result3 + "->" + result4 + "->" + result5);
+        Iterator<Row> rows = sheet.rowIterator();
+        if (rows.hasNext()) {
+            rows.next();
+        }
+        while (rows.hasNext()) {
+            HSSFRow row = (HSSFRow) rows.next();
+            //получаем значение ячеек по номерам столбцов
+            HSSFCell idCell = row.getCell(ID_COLUMN_NUMBER);
+            //получаем строковое значение из ячейки
+            String id = idCell.getStringCellValue();
+            System.out.println("ID: " + id);
+            HSSFCell asianCell = row.getCell(NAME_COLUMN_ASIAN);
+            String asian = asianCell.getStringCellValue();
+            System.out.println("Азия: " + asian);
+            HSSFCell europa = row.getCell(NAME_COLUMN_EUROPA);
+            String phoneNumber = europa.getStringCellValue();
+            System.out.println("Европа: " + phoneNumber);
+        }
+
+//выбор листа _ строки _ столбца
+// преобразование с вновь введеным методом, который определит формат считываемого знвчения
+//        String result0 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(1));
+//        String result1 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(3));
+//        String result2 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(4));
+//        String result3 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(5));
+//        String result4 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(6));
+//        String result5 = getCelltext(wb.getSheetAt(0).getRow(1).getCell(2));
+//        System.out.println(result0 + "->" + result1 + "->" + result2 + "->" + result3 + "->" + result4 + "->" + result5);
     }
-    
+
 // метод для самостоятельного определения формата считываемого значения
-    public static String getCelltext(Cell cell) {
+    public static String getCelltext(HSSFCell cell) {
 
         String result = "";
 
