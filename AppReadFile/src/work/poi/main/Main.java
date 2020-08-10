a2379d37a3d7d392688ff1fe4f072ad50c97526f
Aleks <Aleks@AleKs>
2020-08-10 22:49:11 +0300

    Initial commit

---
+++ AppReadFile/src/work/poi/main/Main.java
@@ -0,0 +1,13 @@
+
+package work.poi.main;
+
+import work.poi.readExcel.ReadFile;
+
+public class Main {
+
+ 
+    public static void main(String[] args) {
+        new ReadFile();   
+        System.exit(0);
+    }    
+}
